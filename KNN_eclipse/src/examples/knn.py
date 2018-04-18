aa5194ab9451d3430faf585d70ab0e832e09f941
博文 胡 <bowenhu@email.arizona.edu>
2018-04-18 14:29:40 -0700

    demension reduction

--- KNN_eclipse/src/examples/knn.py
+++ KNN_eclipse/src/examples/knn.py
@@ -4,6 +4,27 @@ from sklearn.ensemble import BaggingClassifier
 from sklearn.utils import shuffle
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from matplotlib import offsetbox
+from sklearn import manifold, datasets, decomposition, discriminant_analysis
+
+def embedding_plot(X, title):
+    x_min, x_max = np.min(X, axis=0), np.max(X, axis=0)
+    X = (X - x_min) / (x_max - x_min)
+ 
+    plt.figure()
+    ax = plt.subplot(aspect='equal')
+    sc = ax.scatter(X[:,0], X[:,1], lw=0, s=40, c=y/10.)
+ 
+    shown_images = np.array([[1., 1.]])
+    # for i in range(X.shape[0]):
+    #     if np.min(np.sum((X[i] - shown_images) ** 2, axis=1)) < 1e-2: continue
+    #     shown_images = np.r_[shown_images, [X[i]]]
+    #     ax.add_artist(offsetbox.AnnotationBbox(offsetbox.OffsetImage(digits.images[i], cmap=plt.cm.gray_r), X[i]))
+ 
+    plt.xticks([]), plt.yticks([])
+    plt.title(title)
+
 np.set_printoptions(threshold=np.nan)
 
 X_train = np.genfromtxt("X_train.csv",delimiter=',')
@@ -13,7 +34,7 @@ y_test = np.genfromtxt("y_test.csv",delimiter=',')
 X_train, y_train = shuffle(X_train, y_train, random_state=2)
 knn = 20
 
-
+'''
 for weights in ['uniform', 'distance']:
 	#we create an instance of Neighbors Classifier and fit the data.
 	
@@ -21,10 +42,21 @@ for weights in ['uniform', 'distance']:
 	clf.fit(X_train, y_train)
 	Z = clf.score(X_test,y_test)
 	print(Z)
-	
+'''	
 # bClf = BaggingClassifier(KNeighborsClassifier(knn,weights='uniform'),
 # 	n_estimators=10,max_samples=0.7,max_features=0.7)
 # scaler = StandardScaler()
 # X_train = scaler.fit_transform(X_train)
 # bClf.fit(X_train,y_train)
 # print(bClf.score(X_test,y_test))
+
+X = X_train
+y = y_train
+n_samples, n_features = X.shape
+X_pca = decomposition.PCA(n_components=2).fit_transform(X)
+embedding_plot(X_pca, "PCA")
+plt.show()
+
+
+
+
--- KNN_eclipse/src/examples/npV.py
+++ KNN_eclipse/src/examples/npV.py
@@ -1,52 +1,80 @@
-import numpy
+import numpy as np
+import scipy as sc
+import pandas as pd
 
-SIZE = 8
-index = 0
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
 
-def init():
-    plate = numpy.chararray((SIZE, SIZE))
-    plate[:] = '.'
-    return plate
-    
-def iterationSearch():
-    plate = init()
-    setLocation(plate, 0)        
+from sklearn import manifold, datasets, decomposition, discriminant_analysis
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
+import matplotlib.pyplot as plt
+from matplotlib import offsetbox
 
-def setLocation(plate, col):
-    if col >= SIZE:
-        global index
-        index += 1
-        plate[plate == b'x'] = '.'
-        print("No. " + str(index) + ":")
-        print(" "+ numpy.array_str(numpy.core.defchararray.decode(plate)).replace('[','').replace(']','').replace("'",''))
-        print("========================")
-        print()
-    else:
-        for row in range(SIZE):
-            if (b'.' not in plate[:,col]):
-                return
-            elif (plate[row][col] == b'x'):
-                continue
-            else:
-                newPlate = numpy.copy(plate)
-                newPlate[row][col] = '*'
-                setDisable(newPlate, row, col)
-                setLocation(newPlate, col+1)
+#X_train = np.vstack((pd.read_csv("X_train.csv", header=None).values, pd.read_csv("X_test.csv", header=None).values))
+X_test = pd.read_csv("X_test.csv", header=None).values
+#Y_train = np.append(pd.read_csv("Y_train.csv", header=None).values.ravel(), pd.read_csv("Y_test.csv", header=None).values.ravel())
+Y_test = pd.read_csv("Y_test.csv", header=None).values.ravel()
+X_train = pd.read_csv("X_train.csv", header=None).values
+Y_train = pd.read_csv("Y_train.csv", header=None).values.ravel()
+print(X_train.shape)
+X_pca = decomposition.PCA(n_components=2).fit_transform(X_train)
+X_pca_t = decomposition.PCA(n_components=2).fit_transform(X_test)
 
-def setDisable(plate, row, col):
-    plate[row:row+1] = 'x'
-    if (col >= row):
-        numpy.fill_diagonal(plate[row-row:, col - row:], 'x')
-    else:
-        numpy.fill_diagonal(plate[row-col:, col - col:], 'x')
-    if ((SIZE - 1 - col) >= row):    
-        numpy.fill_diagonal(numpy.fliplr(plate)[row - row:, (SIZE - 1 - col) - row:], 'x')
-    else:
-        numpy.fill_diagonal(numpy.fliplr(plate)[row - (SIZE - 1 - col):, (SIZE - 1 - col) - (SIZE - 1 - col):], 'x')
-    plate[row,col] = '*'
-    
-def main():
-    iterationSearch()
-    
-if __name__ == '__main__':
-    main()
+#print(X_train)
+#knn = KNeighborsClassifier(n_neighbors = 46)
+#svm = SVC(kernel="poly", degree = 1)
+#dtree = DecisionTreeClassifier()
+#mlp = MLPClassifier(hidden_layer_sizes = (200,))
+#nbayes = GaussianNB()
+
+#svm.fit(X_train, Y_train)
+#knn.fit(X_train, Y_train)
+#dtree.fit(X_train, Y_train)
+#nbayes.fit(X_train, Y_train)
+
+#pred = knn.predict(X_test)
+#pred = svm.predict(X_test)
+#pred = dtree.predict(X_test)
+#pred = nbayes.predict(X_test)
+#print(pred)
+
+#c = 0
+#for p, t in zip(pred, Y_test):
+
+
+#print(accuracy_score(Y_test, pred))
+
+#neighbors = list(range(1,3))
+#degree = list(range(1,10))
+#neighbors = list(filter(lambda x : x % 5 != 0, myList))
+
+#print(list(neighbors))
+'''
+cv_scores = []
+
+for k in neighbors:
+	print(k)
+	svm = SVC(kernel='poly', degree = k)
+	#knn = KNeighborsClassifier(n_neighbors = k)
+	scores = cross_val_score(svm, X_pca, Y_train, cv = 10, scoring='accuracy')
+	cv_scores.append(scores.mean())
+
+#sc = [0.5539130434782609, 0.5062608695652174, 0.49565217391304345, 0.4882608695652174, 0.4696521739130435, 0.46339130434782605, 0.45582608695652177, 0.4509565217391304]
+
+optimal_k = neighbors[cv_scores.index(max(cv_scores))]
+
+print(optimal_k)
+
+'''
+#X_pca = decomposition.PCA(n_components=2).fit_transform(X)
+#embedding_plot(X_pca, "PCA")
+#plt.show()
+'''
+'''
+#plt.plot(neighbors, cv_scores)
+#plt.show()
